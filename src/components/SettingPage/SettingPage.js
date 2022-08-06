812a608fe2389da8625ffb84a6d51e4cee7bbd9b
mkthebea <5596molly@naver.com>
2022-08-06 20:11:16 +0900

    setting page: user info

--- src/components/SettingPage/SettingPage.js
+++ src/components/SettingPage/SettingPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Input, DatePicker, Tabs, Switch, Button, message, Divider, List, Typography } from "antd";
+import { Input, DatePicker, Tabs, Switch, Button, message, List } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import styles from "./SettingPage.module.css";
 
 function SettingPage() {
@@ -26,7 +27,7 @@ function SettingPage() {
   };
 
   const onChangeTab = (key) => {
-    console.log(key);
+    // console.log(key);
   };
 
   const onChangeSwitchSpam = (checked) => {
@@ -48,20 +49,51 @@ function SettingPage() {
   const saveSetting = () => {
     console.log(value);
     if ((spam && value["spam"] === "") || (time && value["time"] === "") || (word && value["word"] === "")) {
-      message.info("비어있는 값을 입력하세요");
+      message.error("비어있는 값을 입력하세요");
     } else {
-      message.info("저장됨");
+      message.success("저장됨");
     }
   };
 
   // Tab 2
   // const accountsList = ["abcd@naver.com", "1234@daum.net", "qwer@google.com"];
-  const [accountsList, setAccountsList] = useState(["abcd@naver.com", "1234@daum.net", "qwer@google.com"]);
+  const [accountsList, setAccountsList] = useState(["abcd@naver.com", "1234@daum.net", "qwer@google.com", "1@naver.com", "2@naver.com"]);
 
   const onDelete = (item) => {
     // 삭제 요청 보내기!
     console.log("delete: ", item);
-    setAccountsList(accountsList.filter((i) => i !== item));
+    const isSuccess = true; //삭제 요청 응답
+    if (isSuccess) {
+      message.success("계정 삭제에 성공했습니다.");
+      setAccountsList(accountsList.filter((i) => i !== item));
+    } else {
+      message.error("에러 발생");
+    }
+  };
+
+  // Tab3
+  const [userInfo, setUserInfo] = useState({
+    id: "test123",
+    password: "pwpw",
+  });
+
+  const saveInfo = () => {
+    // 저장 요청 보내기
+    console.log(userInfo);
+    const isSuccess = true; // 저장 요청 응답
+    if (isSuccess) {
+      message.success("저장되었습니다.");
+    } else {
+      message.error("에러 발생");
+    }
+  };
+
+  const changeInfo = (key, value) => {
+    setUserInfo((current) => {
+      let newValue = { ...current };
+      newValue[key] = value;
+      return newValue;
+    });
   };
 
   return (
@@ -104,28 +136,51 @@ function SettingPage() {
               </div>
             </div>
             <Button className={styles.save_button} onClick={saveSetting}>
-              Save
+              저장
             </Button>
           </TabPane>
           <TabPane tab="메일 계정 관리" key="2">
-            <List
-              header={<div>메일 계정 목록</div>}
-              // footer={<div>Footer</div>}
-              bordered
-              dataSource={accountsList}
-              renderItem={(item) => (
-                <List.Item className={styles.list_item}>
-                  {/* <Typography.Text mark>[ITEM]</Typography.Text>  */}
-                  {item}
-                  <Button type="text" className={styles.delete_button} onClick={() => onDelete(item)}>
-                    삭제
-                  </Button>
-                </List.Item>
-              )}
-            />
+            <div className={styles.scroll_list}>
+              <List
+                // header={<div>메일 계정 목록</div>}
+                // footer={<div>Footer</div>}
+                // bordered
+                dataSource={accountsList}
+                renderItem={(item) => (
+                  <List.Item className={styles.list_item}>
+                    {/* <Typography.Text mark>[ITEM]</Typography.Text>  */}
+                    {item}
+                    <Button type="text" className={styles.delete_button} onClick={() => onDelete(item)}>
+                      삭제
+                    </Button>
+                  </List.Item>
+                )}
+              />
+            </div>
+            <div className={styles.tab2_button_container}>
+              <Button
+                onClick={() => {
+                  window.location.replace("/register");
+                }}
+              >
+                계정 추가하기
+              </Button>
+            </div>
           </TabPane>
           <TabPane tab="사용자 정보" key="3">
-            사용자 정보
+            <div className={styles.tab3}>
+              <div>
+                <div className={styles.tab3_text_container}>ID</div>
+                <Input size="large" onChange={(event) => changeInfo("id", event.target.value)} defaultValue={userInfo.id} prefix={<UserOutlined />} className={styles.input_box} />
+              </div>
+              <div>
+                <div className={styles.tab3_text_container}>PW</div>
+                <Input size="large" onChange={(event) => changeInfo("password", event.target.value)} defaultValue={userInfo.password} prefix={<UserOutlined />} className={styles.input_box} />
+              </div>
+              <Button className={styles.save_button} onClick={saveInfo}>
+                저장
+              </Button>
+            </div>
           </TabPane>
         </Tabs>
       </div>
