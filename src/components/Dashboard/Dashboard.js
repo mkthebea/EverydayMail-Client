7417d4d90304fa9e1de56c1c44b7bbbcef9831d2
mkthebea <5596molly@naver.com>
2022-08-31 02:44:05 +0900

    show tree steps: show description box when hover

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -19,8 +19,11 @@ function Dashboard() {
     fetchDeletedMails();
   }, []);
 
+  const [descBox, setDescBox] = useState(false);
+  const [descBoxSprout, setDescBoxSprout] = useState(false);
+
   // const co2 = deletedMails * 4;
-  const co2 = 50000;
+  const co2 = 1000;
   const phase = co2 < 500 ? 0 : co2 < 2000 ? 1 : co2 < 4000 ? 2 : 3;
   // const phase = 3;
 
@@ -33,8 +36,46 @@ function Dashboard() {
       </div>
       {/* <img className={styles.trees} src={Tree3} /> */}
       {/* <img className={styles.sprout} src={Sprout} /> */}
-      {phase === 0 ? <img className={styles.sprout} src={Sprout} /> : <img className={styles.trees} src={phase === 1 ? Tree1 : phase === 2 ? Tree2 : Tree3} />}
 
+      {phase === 0 ? (
+        <img
+          className={styles.sprout}
+          src={Sprout}
+          onMouseEnter={() => {
+            setDescBoxSprout(true);
+          }}
+          onMouseLeave={() => {
+            setDescBoxSprout(false);
+          }}
+        />
+      ) : (
+        <img
+          className={styles.trees}
+          src={phase === 1 ? Tree1 : phase === 2 ? Tree2 : Tree3}
+          onMouseEnter={() => {
+            setDescBox(true);
+          }}
+          onMouseLeave={() => {
+            setDescBox(false);
+          }}
+        />
+      )}
+      {descBoxSprout ? (
+        <div className={styles.desc_box_sprout}>
+          <div style={{ color: "white", fontSize: "20px", margin: "10px" }}>현재 단계</div>
+          <div>새싹</div>
+        </div>
+      ) : (
+        <></>
+      )}
+      {descBox ? (
+        <div className={styles.desc_box}>
+          <div style={{ color: "white", fontSize: "20px", margin: "10px" }}>현재 단계</div>
+          <div>나무 {phase}단계</div>
+        </div>
+      ) : (
+        <></>
+      )}
       {/* {co2 < 500 ? <img className={styles.sprout} src={Sprout} /> : <img className={styles.trees} src={co2 < 2000 ? BabyTree : co2 < 4000 ? Tree1 : Orangetree} />} */}
       <div className={styles.ground}>My tree</div>
       <div className={styles.bird_container}>
