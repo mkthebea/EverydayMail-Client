76ae7390df57e4c785e131b942f7ce52c1bbf146
mkthebea <5596molly@naver.com>
2022-09-04 00:29:56 +0900

    403(Not Authorized) page design

--- src/components/NotAuthorized/NotAuthorized.js
+++ src/components/NotAuthorized/NotAuthorized.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Button, Result } from "antd";
+import styles from "./NotAuthorized.module.css";
+
 //접근 권한 없음
 function NotAuthorized() {
-  return <div>NotAuthorized</div>;
+  return (
+    <div className={styles.container}>
+      <Result
+        status="403"
+        title="403"
+        subTitle="Sorry, you are not authorized to access this page. Please log in to access."
+        extra={
+          <Button
+            type="primary"
+            onClick={() => {
+              window.location.replace("/login");
+            }}
+          >
+            Go Login
+          </Button>
+        }
+      />
+    </div>
+  );
 }
 
 export default NotAuthorized;
